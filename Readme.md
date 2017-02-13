67569a9a7e95d4354c88acc181dcb1a1881e5299
Piotr <piotr.borysowski@gmail.com>
2017-02-13 19:05:16 +0100

    Add read function with covered tests

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Test
+
+npm run testNyan
+
+npm test
---
+++ index.js
@@ -0,0 +1,69 @@
+function create(sheet, limit, offset, search) {
+}
+
+function read(limit, offset, search, sheet) {
+  var config = this.config;
+
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    var sheetParam = (!sheet) ? 'Sheet1' : sheet;
+    var limitParam = (!limit) ? '' : '?limit=' + limit;
+    var offsetParam = (!offset) ? '' : '?offset=' + offset;
+    var searchParam = (!search) ? '' : '/search';
+    var searchKeys = (!search) ? [] : Object.keys(search);
+
+    for (var i = 0; i < searchKeys.length; i++) {
+        var searchValue = search[searchKeys[i]];
+        if(i === 0){
+          searchParam += '?' + searchKeys[i] + '=' + searchValue;
+        }
+        else {
+          searchParam += '&' + searchKeys[i] + '=' + searchValue;
+        }
+    }
+
+    var url = config.address + '/sheets/' + sheetParam + limitParam + offsetParam + searchParam;
+
+    xhr.open("GET", url, true);
+
+    xhr.onload = function (e) {
+      if (xhr.readyState === 4) {
+        resolve(xhr);
+      }
+    };
+
+    xhr.onerror = function (e) {
+      reject(e);
+    };
+
+    xhr.send(null);
+  });
+}
+
+function update() {
+}
+
+function deleteFunc() {
+}
+
+var sheetsuNode = function(config) {
+  var configParam = config || {};
+
+  var version = configParam.version || '1.0';
+  var api_key = configParam.api_key || '';
+  var api_secret = configParam.api_secret || '';
+  if(!configParam.address) {
+    throw Error('address param needed');
+  }
+  var address = configParam.address;
+
+  return {
+    config: configParam,
+    create: create,
+    read: read,
+    update: update,
+    delete: deleteFunc,
+  }
+}
+
+module.exports = sheetsuNode;
