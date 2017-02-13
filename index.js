d0fd7d9179c0ae749421405711fbc4c8c87c50bb
Piotr <piotr.borysowski@gmail.com>
2017-02-13 21:23:50 +0100

    Add create and delete functions with covered tests

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-function create(sheet, limit, offset, search) {
+function create(newObject, sheet) {
+  var config = this.config;
+
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();;
+    var sheetParam = (!sheet) ? 'Sheet1' : sheet;
+    var isArray = Array.isArray(newObject);
+    var data;
+
+    if (isArray) {
+      data = JSON.stringify({
+        rows: newObject,
+      });
+    } else {
+      data = JSON.stringify(newObject);
+    }
+
+    var url = config.address + '/sheets/' + sheetParam;
+
+    xhr.open("POST", url, true);
+
+    xhr.onload = function (e) {
+      if (xhr.readyState === 4) {
+        resolve(xhr);
+      }
+    };
+
+    xhr.onerror = function (e) {
+      reject(e);
+    };
+
+    xhr.send(data);
+  });
 }
 
 function read(limit, offset, search, sheet) {
@@ -13,13 +45,13 @@ function read(limit, offset, search, sheet) {
     var searchKeys = (!search) ? [] : Object.keys(search);
 
     for (var i = 0; i < searchKeys.length; i++) {
-        var searchValue = search[searchKeys[i]];
-        if(i === 0){
-          searchParam += '?' + searchKeys[i] + '=' + searchValue;
-        }
-        else {
-          searchParam += '&' + searchKeys[i] + '=' + searchValue;
-        }
+      var searchValue = search[searchKeys[i]];
+      if(i === 0){
+        searchParam += '?' + searchKeys[i] + '=' + searchValue;
+      }
+      else {
+        searchParam += '&' + searchKeys[i] + '=' + searchValue;
+      }
     }
 
     var url = config.address + '/sheets/' + sheetParam + limitParam + offsetParam + searchParam;
@@ -43,7 +75,33 @@ function read(limit, offset, search, sheet) {
 function update() {
 }
 
-function deleteFunc() {
+function deleteFunc(columnName, value, sheet) {
+  var config = this.config;
+
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();;
+    var sheetParam = (!sheet) ? 'Sheet1' : sheet;
+
+    if(!columnName) {
+      reject('no column name');
+    }
+
+    var url = config.address + '/sheets/' + sheetParam + '/' + columnName+  '/' + value;
+
+    xhr.open("DELETE", url, true);
+
+    xhr.onload = function (e) {
+      if (xhr.readyState === 4) {
+        resolve(xhr);
+      }
+    };
+
+    xhr.onerror = function (e) {
+      reject(e);
+    };
+
+    xhr.send();
+  });
 }
 
 var sheetsuNode = function(config) {
--- test/test.js
+++ test/test.js
@@ -4,16 +4,14 @@ var MockXMLHttpRequest  =  require('mock-xmlhttprequest');
 global.XMLHttpRequest = MockXMLHttpRequest;
 
 describe('sheetsu', function() {
-  describe('config', function() {
-    it('should throw error when no address', function() {
-      assert.throws(function(){
+  describe('contructor', function() {
+    it('should throw error when param has no address', function() {
+      assert.throws(function() {
         sheetsuAPI();
       }, Error);
     });
-    it('should not throw error when is address', function() {
-      sheetsuAPI.bind(sheetsuAPI, {
-        address: 'http://',
-      });
+
+    it('should not throw error when param has address', function() {
       assert.doesNotThrow(function() {
         sheetsuAPI({
           address: 'http://',
@@ -22,7 +20,7 @@ describe('sheetsu', function() {
     });
   });
 
-  describe('read function without security tokens', function() {
+  describe('read function run without security token', function() {
     var sheetsu = sheetsuAPI({
       address: 'http://testAddress',
     });
@@ -37,10 +35,16 @@ describe('sheetsu', function() {
       xhr.respond(200, responseHeaders, JSON.stringify(response));
     };
 
+    it('should run with GET method', function() {
+      return sheetsu.read().then(function(data){
+        assert.equal(data.method, 'GET');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+
     it('should return url without limit and offset', function() {
       return sheetsu.read().then(function(data){
-        var url = data.url;
-
         assert.equal(data.url, 'http://testAddress/sheets/Sheet1');
       }, function(err) {
         assert.fail('sheetsu throw error');
@@ -49,8 +53,6 @@ describe('sheetsu', function() {
 
     it('should return url with limit', function() {
       return sheetsu.read(5).then(function(data){
-        var url = data.url;
-
         assert.equal(data.url, 'http://testAddress/sheets/Sheet1?limit=5');
       }, function(err) {
         assert.fail('sheetsu throw error');
@@ -58,9 +60,7 @@ describe('sheetsu', function() {
     });
 
     it('should return url with offset', function() {
-      return sheetsu.read(undefined, 10).then(function(data){
-        var url = data.url;
-
+      return sheetsu.read(undefined, 10).then(function(data) {
         assert.equal(data.url, 'http://testAddress/sheets/Sheet1?offset=10');
       }, function(err) {
         assert.fail('sheetsu throw error');
@@ -69,8 +69,6 @@ describe('sheetsu', function() {
 
     it('should return url with offset and limit', function() {
       return sheetsu.read(5, 10).then(function(data){
-        var url = data.url;
-
         assert.equal(data.url, 'http://testAddress/sheets/Sheet1?limit=5?offset=10');
       }, function(err) {
         assert.fail('sheetsu throw error');
@@ -78,19 +76,114 @@ describe('sheetsu', function() {
     });
 
     it('should be able to search', function() {
-      return sheetsu.read(undefined, undefined, {name: 'test', foo: 'bar'}).then(function(data){
-        var url = data.url;
+      return sheetsu.read(undefined, undefined, {name: 'test', foo: 'bar'}).then(function(data) {
         assert.equal(data.url, 'http://testAddress/sheets/Sheet1/search?name=test&foo=bar');
       }, function(err) {
         assert.fail('sheetsu throw error');
       });
     });
 
-    it('should use different sheet', function() {
+    it('should be able to use different sheet', function() {
       return sheetsu.read(undefined, undefined, undefined, 'Sheet3').then(function(data){
         var url = data.url;
 
         assert.equal(data.url, 'http://testAddress/sheets/Sheet3');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+  });
+
+  describe('create function run without security token', function() {
+    var sheetsu = sheetsuAPI({
+      address: 'http://testAddress',
+    });
+
+    MockXMLHttpRequest.onSend = function(xhr) {
+    var response = {
+      result: 'success',
+    };
+    var responseHeaders = {
+      'Content-Type': 'application/json',
+    }
+      xhr.respond(200, responseHeaders, JSON.stringify(response));
+    };
+
+    it('should run with POST method', function() {
+      return sheetsu.create({}).then(function(data) {
+        assert.equal(data.method, 'POST');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+
+    it('should run with object data', function() {
+      return sheetsu.create({some: 5}).then(function(data){
+        assert.equal(data.body, '{"some":5}');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+
+    it('should run with array data', function() {
+      return sheetsu.create([{}, {test: 3}]).then(function(data){
+        assert.equal(data.body, '{"rows":[{},{"test":3}]}');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+
+    it('should return url different Sheet', function() {
+      return sheetsu.create(undefined, 'Sheet2').then(function(data){
+        assert.equal(data.url, 'http://testAddress/sheets/Sheet2');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+
+  });
+  describe('delete function run without security token', function() {
+    var sheetsu = sheetsuAPI({
+      address: 'http://testAddress',
+    });
+
+    MockXMLHttpRequest.onSend = function(xhr) {
+    var response = {
+      result: 'success',
+    };
+    var responseHeaders = {
+      'Content-Type': 'application/json',
+    }
+      xhr.respond(200, responseHeaders, JSON.stringify(response));
+    };
+
+    it('should run with DELETE method', function() {
+      return sheetsu.delete('column', 'test').then(function(data) {
+        assert.equal(data.method, 'DELETE');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+
+    it('should run with column name and value', function() {
+      return sheetsu.delete('column', 'value').then(function(data){
+        assert.equal(data.url, 'http://testAddress/sheets/Sheet1/column/value');
+      }, function(err) {
+        assert.fail('sheetsu throw error');
+      });
+    });
+
+    it('should throw error when no column param', function() {
+      return sheetsu.delete().then(function(data){
+        assert.fail('sheetsu do not throw error');
+      }, function(err) {
+        assert.equal(err, 'no column name');
+      });
+    });
+
+    it('should return url different Sheet', function() {
+      return sheetsu.delete('column', 'test', 'Sheet2').then(function(data){
+        assert.equal(data.url, 'http://testAddress/sheets/Sheet2/column/test');
       }, function(err) {
         assert.fail('sheetsu throw error');
       });
